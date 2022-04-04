713e86d50123fc7732c0c4e4273884f4297fb640
Tiffany Chen <tiffanysta.c@gmail.com>
2022-04-04 15:58:31 -0400

    push push

--- public/js/streetview.js
+++ public/js/streetview.js
@@ -9,6 +9,18 @@ function initStreetView() {
         document.getElementById('pano')
     );
 
+    var marker = new google.maps.Marker({
+        position: stata,
+        panorama,
+        title: "Hello World!",
+      });
+
+    marker.setMap(panorama);
+
+    panorama.addListener('click', (event) => {
+        console.log('click at ' + event.latLng);
+    });
+
     // set initial sv camera
     sv.getPanorama({ location: stata, radius: 50 }).then(processSVData);
 }
@@ -24,12 +36,101 @@ function processSVData({ data }) {
     panorama.setVisible(true);
 }
 
-var cursor;
+function click(x, y) {
+    var ev = new MouseEvent('click', {
+        'view': window,
+        'bubbles': true,
+        'cancelable': true,
+        'screenX': x,
+        'screenY': y
+    });
+
+    el = document.elementFromPoint(x, y);
+
+    el.dispatchEvent(ev);
+
+    console.log(ev);
+}
+
+
+// Initialize cursor position
+window.addEventListener('load', () => {
+    let cursor = document.getElementById('cursor');
+    cursor.style.position = 'absolute';
+    cursor.style.left = 0;
+    cursor.style.top = 0;
+});
+
 var cursorPosition;
+var gestureTimer = false;
+
+function changeRotation(hand) {
+    var palmVelX = hand.palmVelocity[0];
+    var palmVelY = hand.palmVelocity[1];
+        var xDirection = Math.abs(palmVelX) > Math.abs(palmVelY);
+        var xChange = xDirection ? 5 : 0;
+        var yChange = xDirection ? 0 : 5;
+        if (palmVelX > 0) xChange *= -1;
+        if (palmVelY > 0) yChange *= -1;
+        var newHeading = panorama.getPov().heading + xChange;
+        if (newHeading >= 360) newHeading -= 360;
+        else if (newHeading < 0) newHeading += 360;
+        var newPitch = panorama.getPov().pitch + yChange;
+        if (newPitch >= 90) newPitch = 90;
+        else if (newPitch <= -90) newPitch = -90;
+        gestureTimer = true;
+        panorama.setPov({
+            heading: newHeading,
+            pitch: newPitch
+        });
+        setTimeout(() => gestureTimer = false , 50 );
+}
+
+function changeZoom(hand) {
+    return;
+}
+
+function changePosition(hand) {
+        currHeading = panorama.getPov().heading;
+        // console.log(currHeading);
+        var links = panorama.getLinks();
+
+        for (let link of links) {
+            link.diff = Math.abs(currHeading - link.heading);
+        }
+        links = links.sort((obj1, obj2) => obj1.diff - obj2.diff);
+
+        var newPano = links[0].pano;
+        panorama.setPano(newPano);
+
+        gestureTimer = true;
+        setTimeout(() => gestureTimer = false , 50 );
+}
 
 // Main loop
 Leap.loop({ hand: function(hand) {
-    cursorPosition = hand.screenPosition();
-    cursor.setScreenPosition(cursorPosition);
-    console.log(cursorPosition);
-}}).use('screenPosition', {scale: LEAPSCALE});
+    // var cursorPosition = hand.screenPosition();
+    // let cursor = document.getElementById('cursor');
+    // var x = parseInt(cursorPosition[0]);
+    // var y = parseInt(cursorPosition[1]);
+    // if (x > 0 && y > 0) {
+    //     cursor.style.left = x + 'px';
+    //     cursor.style.top = y + 'px';
+    //     if (hand.grabStrength > 0.9) {
+    //         click(x, y);
+    //     }
+    // }
+
+    var pointing = hand.indexFinger.extended && !(hand.thumb.extended) && !(hand.pinky.extended) && !(hand.middleFinger.extended) && !(hand.ringFinger.extended);
+    var palmVel = hand.palmVelocity;
+    var palmVelX = palmVel[0];
+    var palmVelY = palmVel[1];
+    var velMag = (palmVelX ** 2 + palmVelY ** 2) ** 0.5;
+    
+    if (gestureTimer) return;
+
+    if (pointing) changePosition(hand);
+    else if (velMag > 100 && hand.grabStrength > 0.9) changeRotation(hand);
+    
+
+}}).use('screenPosition');
